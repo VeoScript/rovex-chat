794539a9af3aa37ac7e71367bdfa5e91752e71b5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-01 14:52:30 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    screens: {
-      'sm': {'min': '320px', 'max': '568px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
-    },
   },
   variants: {
     extend: {},
