42bc11cd713ab5bf1b85de4063689a452c76d9b4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-01 14:52:22 +0800

    Create chatroom.vue

---
+++ pages/chatroom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-18 lg:w-60 flex-none bg-yellow-50 flex flex-col h-screen">
+    <h1>This is Chatroom</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Rovexchat | Chatroom'
+    }
+  }
+}
+</script>
