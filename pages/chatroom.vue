0ffd3bc2297b76d9357aa8f14fb2cf52425733fa
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-18 16:30:56 +0800

    Update chatroom.vue

--- pages/chatroom.vue
+++ pages/chatroom.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="w-18 lg:w-60 flex-none bg-yellow-50 flex flex-col h-screen">
-    <h1>This is Chatroom</h1>
+  <div>
+    <div class="w-18 lg:w-60 flex-none bg-yellow-50 flex flex-col h-screen">
+      <h1>This is Chatroom</h1>
+    </div>
   </div>
 </template>
 
