58c2887e846857d379705f54eb15f971b4aa986a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-20 11:48:47 +0800

    change size and margin

--- pages/chatroom.vue
+++ pages/chatroom.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="w-64 xs:hidden sm:hidden md:hidden lg:hidden bg-gray-900 h-screen text-white">
       <div class="flex-shrink-0 flex justify-center">
-        <img class="w-72" src="~/assets/img/rovex_w.png" alt="avatar" />
+        <img class="w-52 mt-5" src="~/assets/img/rovex_w.png" alt="avatar" />
       </div>
       <div class="flex-shrink-0 flex justify-center">
         <p class="text-xs text-center">Private conversation, find your partner now.</p>
