d2014f186e8a8721d7ace6bc87bc1182e9e6375d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-20 11:42:41 +0800

    set new breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'xs': {'min': '250px', 'max': '475px'},
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
+      'xs': {'max': '250px'},
+      'sm': {'max': '320px'},
+      'md': {'max': '768px'},
+      'lg': {'max': '1024px'},
+      'xl': {'max': '1280px'},
+      '2xl': {'max': '1536px'},
     },
   },
   variants: {
