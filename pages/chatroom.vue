b68b9234c5987b29116008f498d07898b6ae954a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-20 11:43:01 +0800

    added rovexchat logo in sidebar

--- pages/chatroom.vue
+++ pages/chatroom.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <div class="w-60 xs:hidden bg-yellow-50 h-screen">
-      <h1>This is Chatroom</h1>
+    <div class="w-64 xs:hidden sm:hidden md:hidden lg:hidden bg-gray-900 h-screen text-white">
+      <div class="flex-shrink-0 flex justify-center">
+        <img class="w-72" src="~/assets/img/rovex_w.png" alt="avatar" />
+      </div>
+      <div class="flex-shrink-0 flex justify-center">
+        <p class="text-xs text-center">Private conversation, find your partner now.</p>
+      </div>
     </div>
   </div>
 </template>
